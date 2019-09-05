de379720916c2b246ee9a62e83d43f3b4849b9da
szagr <szagr@softserveinc.com>
2019-09-05 13:40:06 +0300

    create Menu items models

---
+++ src/main/java/com/serzh/domain/Item.java
@@ -0,0 +1,48 @@
+package com.serzh.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "item")
+public class Item implements Serializable {
+
+    private static final long serialVersionUID = -8137927479202281169L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(length = 50, unique = true, nullable = false)
+    private String name;
+
+    private String description;
+
+    private String image;
+
+    @Column(scale = 5, precision = 20, nullable = false)
+    private BigDecimal price;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "item_types",
+            joinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_item_types_item")),
+            inverseJoinColumns = @JoinColumn(name = "type_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_item_types_type"))
+    )
+    private Set<Type> types = new HashSet<>();
+
+}
---
+++ src/main/java/com/serzh/domain/Type.java
@@ -0,0 +1,31 @@
+package com.serzh.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "type")
+public class Type implements Serializable {
+
+    private static final long serialVersionUID = -8137927479202284269L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, unique = true, nullable = false)
+    private String name;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(mappedBy = "types")
+    private Set<Item> items = new HashSet<>();
+
+}
